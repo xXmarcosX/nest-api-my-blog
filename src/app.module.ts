b0f76206cbe7d0aaf55475195143618560c6097d
xXmarcosX <marcos.v3567@gmail.com>
2026-05-25 21:35:18 -0300

    add: user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UserModule,
+    PostModule,
+    ConfigModule.forRoot({ 
+      isGlobal: true 
+    })
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
